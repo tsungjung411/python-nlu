a9493e4448cae4100367d6afa283f5b5dfae9e6a
tsungjung411 <tsungjung411@yahoo.com.tw>
2018-08-02 00:08:52 +0800

    Create 千.py

---
+++ semantic-parsing/tagger/zh_rTW/math/unit/metric_prefix/千.py
@@ -0,0 +1,22 @@
+from tagger.zh_rTW.math.unit.metric_prefix.metric_prefix_unit import _MetricPrefixUnit
+from tagger.zh_rTW.math.unit.metric_prefix.百 import 百
+
+class 千(_MetricPrefixUnit):
+    '''
+    @since 2018.07.25
+    @author tsungjung411@gmail.com
+    '''
+    
+    def get_synonym_list(self):
+        return ['千']
+    # end-of-def
+    
+    def get_unit_size(self):
+        return 1e3 # =1000
+    # end-def
+    
+    def get_dependent_tagger_list(self):
+        return [百]
+    # end-of-def
+    
+# end-of-class
