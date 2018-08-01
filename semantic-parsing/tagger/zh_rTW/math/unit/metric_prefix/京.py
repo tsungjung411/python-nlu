2eece57f6cd7dc74f1a6d847f93d1ddda4a7bb06
tsungjung411 <tsungjung411@yahoo.com.tw>
2018-08-02 00:10:28 +0800

    Create 京.py

---
+++ semantic-parsing/tagger/zh_rTW/math/unit/metric_prefix/京.py
@@ -0,0 +1,22 @@
+from tagger.zh_rTW.math.unit.metric_prefix.metric_prefix_unit import _MetricPrefixUnit
+from tagger.zh_rTW.math.unit.metric_prefix.兆 import 兆
+
+class 京(_MetricPrefixUnit):
+    '''
+    @since 2018.07.25
+    @author tsungjung411@gmail.com
+    '''
+    
+    def get_synonym_list(self):
+        return ['京']
+    # end-of-def
+    
+    def get_unit_size(self):
+        return 1e16 # =10000000000000000
+    # end-def
+    
+    def get_dependent_tagger_list(self):
+        return [兆]
+    # end-of-def
+    
+# end-of-class
