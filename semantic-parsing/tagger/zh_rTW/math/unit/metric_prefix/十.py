07448737a0367e894cdac03d74fd69e6b5915248
tsungjung411 <tsungjung411@yahoo.com.tw>
2018-08-02 00:08:07 +0800

    Create 十.py

---
+++ semantic-parsing/tagger/zh_rTW/math/unit/metric_prefix/十.py
@@ -0,0 +1,17 @@
+from tagger.zh_rTW.math.unit.metric_prefix.metric_prefix_unit import _MetricPrefixUnit
+
+class 十(_MetricPrefixUnit):
+    '''
+    @since 2018.07.25
+    @author tsungjung411@gmail.com
+    '''
+    
+    def get_synonym_list(self):
+        return ['十']
+    # end-of-def
+    
+    def get_unit_size(self):
+        return 1e1 # =10
+    # end-def
+    
+# end-of-class
