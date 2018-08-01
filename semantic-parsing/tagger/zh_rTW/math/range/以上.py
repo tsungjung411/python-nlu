a1d9c4b04217621451b2efbe6d2892f82598801f
tsungjung411 <tsungjung411@yahoo.com.tw>
2018-08-02 00:17:28 +0800

    Create 以上.py

---
+++ semantic-parsing/tagger/zh_rTW/math/range/以上.py
@@ -0,0 +1,13 @@
+from tagger.math.range.gt_eq_prerange import _GtEqPrerange
+
+class 以上(_GtEqPrerange):
+    '''
+    @since 2018.08.01
+    @author tsungjung411@gmail.com
+    '''
+    
+    def get_synonym_list(self):
+        return ['以上']
+    # end-of-def
+    
+# end-of-class
