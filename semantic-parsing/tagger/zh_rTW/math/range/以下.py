5b1d1c637a612e678015db40a4eaea9365f0d45b
tsungjung411 <tsungjung411@yahoo.com.tw>
2018-08-02 00:17:08 +0800

    Create 以下.py

---
+++ semantic-parsing/tagger/zh_rTW/math/range/以下.py
@@ -0,0 +1,13 @@
+from tagger.math.range.lt_eq_prerange import _LtEqPrerange
+
+class 以下(_LtEqPrerange):
+    '''
+    @since 2018.08.01
+    @author tsungjung411@gmail.com
+    '''
+    
+    def get_synonym_list(self):
+        return ['以下']
+    # end-of-def
+    
+# end-of-class
