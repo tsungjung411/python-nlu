e171de869dbba9c4f6e81b6a68ac3f9c6a4dc741
tsungjung411 <tsungjung411@yahoo.com.tw>
2018-08-02 00:02:06 +0800

    Create mega.py

---
+++ semantic-parsing/tagger/math/unit/metric_prefix/mega.py
@@ -0,0 +1,19 @@
+from tagger.math.unit.metric_prefix.metric_prefix_unit import _MetricPrefixUnit
+
+class mega(_MetricPrefixUnit):
+    '''
+    Define the mega unit.
+    
+    @since 2018.07.25
+    @author tsungjung411@gmail.com
+    '''
+    
+    def get_synonym_list(self):
+        return ['m', 'mega'] 
+    # end-of-def
+    
+    def get_unit_size(self):
+        return 1e6
+    # end-def
+    
+# end-of-class
