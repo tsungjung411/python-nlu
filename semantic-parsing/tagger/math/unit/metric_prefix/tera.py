0f74bb246168cffa1e5fd42813c91c6e4d8d3821
tsungjung411 <tsungjung411@yahoo.com.tw>
2018-08-02 00:02:48 +0800

    Create tera.py

---
+++ semantic-parsing/tagger/math/unit/metric_prefix/tera.py
@@ -0,0 +1,19 @@
+from tagger.math.unit.metric_prefix.metric_prefix_unit import _MetricPrefixUnit
+
+class tera(_MetricPrefixUnit):
+    '''
+    Define the tera unit.
+    
+    @since 2018.07.25
+    @author tsungjung411@gmail.com
+    '''
+    
+    def get_synonym_list(self):
+        return ['t', 'tera'] 
+    # end-of-def
+    
+    def get_unit_size(self):
+        return 1e12
+    # end-def
+    
+# end-of-class
