dd0d2ebe5748a78f758809d8f27f774d3f914c23
tsungjung411 <tsungjung411@yahoo.com.tw>
2018-08-02 00:03:15 +0800

    Create deca.py

---
+++ semantic-parsing/tagger/math/unit/metric_prefix/deca.py
@@ -0,0 +1,19 @@
+from tagger.math.unit.metric_prefix.metric_prefix_unit import _MetricPrefixUnit
+
+class deca(_MetricPrefixUnit):
+    '''
+    Define the deca unit.
+    
+    @since 2018.07.25
+    @author tsungjung411@gmail.com
+    '''
+    
+    def get_synonym_list(self):
+        return ['da', 'deca', 'deka']
+    # end-of-def
+    
+    def get_unit_size(self):
+        return 1e1 # =10
+    # end-def
+    
+# end-of-class
