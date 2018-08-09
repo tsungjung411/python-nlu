c7aff2cdc341edbece647d69f4490dd2a2137f76
tsungjung411 <tsungjung411@yahoo.com.tw>
2018-08-09 22:57:42 +0800

    Add files via upload

---
+++ semantic-parsing/tagger/zh_rTW/adj/_adj.py
@@ -0,0 +1,12 @@
+from tagger._token import _Token
+
+class _adj(_Token):
+    '''
+    Defines the abstract universal-adjective for tagging.
+    
+    @since 2018.08.09
+    @author tsungjung411@gmail.com
+    '''
+    
+    pass
+# end-of-class
---
+++ semantic-parsing/tagger/zh_rTW/adj/_base_adj.py
@@ -0,0 +1,11 @@
+from tagger.zh_rTW.adj._adj import _adj
+
+class _base_adj(_adj):
+    '''
+    Defines the Chinese adjective for tagging.
+    
+    @since 2018.08.09
+    @author tsungjung411@gmail.com
+    '''
+    pass
+# end-of-class
